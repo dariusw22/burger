3d204bf285d91bf3fdd642aea8a4f64c0e7bfa6b
dariusw22 <dariusw22.dw@gmail.com>
2018-03-13 12:45:36 -0400

    adding working server in server.js file and adding updated schema.sql file with new burgers table

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Create the database movie_planner_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;;
+
+-- Create the table plans.
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BIT 1,
+PRIMARY KEY (id)
+);
+
+-- Insert into burgers table dynamicly from the submit button from the index.handlebars file
+INSERT INTO burgers (movie) VALUES ('Rush Hour 2');
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require('express');
-var body-parser = require('body-parser');
+var bodyparser = require('body-parser');
+const http = require('http');
 
 // Create an instance of the express app.
 const app = express();
@@ -9,12 +10,21 @@ const app = express();
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
+// setting up the connection for the server.
+const server = http.createServer(function(req, res) {
+    res.end("you are connected to your local host Darius " + req.url)
+});
+
+// making the connection so the server can listen.
+server.listen(PORT, function() {
+    console.log("server is listing on http://localhost:" + PORT)
+})
+
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // data array
-
 const data = [
     // making the JSON object for the data to live with the perimeters
     {},
