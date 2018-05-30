558b7575ead98b7002ed055e28928dd04d998290
dariusw22 <dariusw22.dw@gmail.com>
2018-05-30 10:10:27 -0400

    finsished index.js file in the javascript

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -112,4 +112,16 @@ $(document).ready(function() {
                 }
             });
     }
+
+    function handleArticleScrape() {
+        // This function handles the user clicking any "scrape new article" buttons
+        $.get("/api/fetch").then(function(data) {
+                  // If we are able to successfully scrape the NYTIMES and compare the articles to those
+                  // already in our collection, re render the articles on the page
+                  // and let the user know how many unique articles we were able to save
+                  initPage();
+                  bootbox.alert("<h3 class='text-center m-top-80>" + data.message + "<h3>");
+
+        });
+    }
 });
