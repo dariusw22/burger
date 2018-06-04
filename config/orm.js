9054525b2fe0874074791168dfcca63f4c8ea06e
dariusw22 <dariusw22.dw@gmail.com>
2018-06-04 12:24:41 -0400

    adding full app with working localhost

--- config/orm.js
+++
@@ -1,7 +0,0 @@
-// Import MySQL connection.
-const connection = require("../config/connection.js");
-
-
-
-// Export the orm object for the model
-module.exports = orm;
--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burger (name) VALUES ('Bar Burger');
-INSERT INTO burger (name) VALUES ('Triple bacon Burger');
-INSERT INTO burger (name) VALUES ('Buffalo Wing Burger');
-
